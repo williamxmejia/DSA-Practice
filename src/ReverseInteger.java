ef691fe83a4e64f0c411a5950c3c61a51e6e7057
William Mejia <willx1995@gmail.com>
2022-06-12 22:37:43 -0500

    feat: reverse integer

---
+++ src/ReverseInteger.java
@@ -0,0 +1,21 @@
+public class ReverseInteger {
+    public int reverseInteger(int x){
+        int reversed = 0;
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        while(x != 0 ) {
+            int lastDigit = x % 10;
+            //min,mx
+
+            if(reversed > max/10 || (reversed == max/10 && lastDigit > 7))
+                return 0;
+
+            if(reversed < min/10 || (reversed == max/10 && lastDigit < -8))
+                return 0;
+            reversed = reversed * 10 + lastDigit;
+
+            x = x /10;
+        }
+        return reversed;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -4,9 +4,16 @@ public class Main {
 //
 //        System.out.println(fizzBuzz.fizzBuzz(100));
 
-        SingleNumber singleNumber = new SingleNumber();
-        System.out.println(singleNumber.SingleNumber(new int[]{1, 2, 2, 3, 3}));
-        System.out.println(singleNumber.SingleNumber(new int[]{1, 1, 2, 4, 4}));
-        System.out.println(singleNumber.SingleNumber(new int[]{2, 2, 5, 5, 1}));
+//        SingleNumber singleNumber = new SingleNumber();
+//        System.out.println(singleNumber.SingleNumber(new int[]{1, 2, 2, 3, 3}));
+//        System.out.println(singleNumber.SingleNumber(new int[]{1, 1, 2, 4, 4}));
+//        System.out.println(singleNumber.SingleNumber(new int[]{2, 2, 5, 5, 1}));
+
+//        ReverseInteger reverseInteger = new ReverseInteger();
+//
+//        System.out.println(reverseInteger.reverseInteger(123));
+//        System.out.println(reverseInteger.reverseInteger(-123));
+//        System.out.println(reverseInteger.reverseInteger(120));
+
     }
 }
