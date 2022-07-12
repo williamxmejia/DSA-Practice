9ac16c98f440a206e3b7fcf92fbb0973922ffe9c
williamxmejia <willx1995@gmail.com>
2022-07-12 00:40:28 -0500

    6kyu GoodVsEvil

---
+++ src/GoodVsEvil.java
@@ -0,0 +1,98 @@
+import java.util.HashMap;
+
+public class GoodVsEvil {
+    public static String battle(String goodAmounts, String evilAmounts) {
+        int goodSum = 0;
+        int evilSum = 0;
+
+        String[] goodGuys = {
+                "Hobbits",
+                "Men",
+                "Elves",
+                "Dwarves",
+                "Eagles",
+                "Wizards"
+        };
+
+        String[] evilGuys = {
+                "Orcs",
+                "Men",
+                "Wargs",
+                "Goblins",
+                "Uruk Hai",
+                "Trolls",
+                "Wizards"
+        };
+
+        String[] goodArray = goodAmounts.split("\\s");
+        String[] evilArray = evilAmounts.split("\\s");
+        HashMap<String, Integer> good = new HashMap<>();
+        HashMap<String, Integer> evil = new HashMap<>();
+
+        for (int i = 0; i < 6; i++) {
+
+            if (goodGuys[i] == "Hobbits") {
+                good.put(goodGuys[i], Integer.valueOf(goodArray[i]));
+            }
+
+            if (goodGuys[i] == "Men") {
+                good.put(goodGuys[i], (Integer.valueOf(goodArray[i]) * 2));
+            }
+
+            if (goodGuys[i] == "Elves" || goodGuys[i] == "Dwarves") {
+                good.put(goodGuys[i], (Integer.valueOf(goodArray[i]) * 3));
+            }
+
+            if (goodGuys[i] == "Eagles") {
+                good.put(goodGuys[i], (Integer.valueOf(goodArray[i]) * 4));
+            }
+
+            if (goodGuys[i] == "Wizards") {
+                good.put(goodGuys[i], (Integer.valueOf(goodArray[i]) * 10));
+            }
+
+            goodSum += good.get(goodGuys[i]);
+
+        }
+
+        System.out.println(goodSum);
+
+
+        for (int i = 0; i < 7; i++) {
+
+            if (evilGuys[i] == "Orcs") {
+                evil.put(evilGuys[i], Integer.valueOf(evilArray[i]));
+            }
+
+            if (evilGuys[i] == "Men" || evilGuys[i] == "Wargs" || evilGuys[i] == "Goblins") {
+                evil.put(evilGuys[i], (Integer.valueOf(evilArray[i]) * 2));
+            }
+
+            if (evilGuys[i] == "Uruk Hai") {
+                evil.put(evilGuys[i], (Integer.valueOf(evilArray[i]) * 3));
+            }
+
+            if (evilGuys[i] == "Trolls") {
+                evil.put(evilGuys[i], (Integer.valueOf(evilArray[i]) * 5));
+            }
+
+            if (evilGuys[i] == "Wizards") {
+                evil.put(evilGuys[i], (Integer.valueOf(evilArray[i]) * 10));
+            }
+
+            evilSum += evil.get(evilGuys[i]);
+        }
+
+        System.out.println(evilSum);
+
+
+
+        if (goodSum > evilSum) {
+            return "Battle Result: Good triumphs over Evil";
+        } else if (evilSum > goodSum) {
+            return "Battle Result: Evil eradicates all trace of Good";
+        } else {
+            return "Battle Result: No victor on this battle field";
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -34,9 +34,16 @@ public class Main {
 //
 //        System.out.println(maxDiffLength.mxdiflg(a1, a2));
 
-        Kata kata = new Kata();
-        int [] arr = {1,2,3,4,3,2,1};
+//        Kata kata = new Kata();
+//        int [] arr = {1,2,3,4,3,2,1};
+//
+//        System.out.println(kata.findEvenIndex(arr));
 
-        System.out.println(kata.findEvenIndex(arr));
+        GoodVsEvil goodVsEvil = new GoodVsEvil();
+//        System.out.println(goodVsEvil.battle("1 1 1 1 1 1", "1 1 1 1 1 1 1"));
+//        System.out.println(goodVsEvil.battle("0 0 0 0 0 10", "0 1 1 1 1 0 0"));
+//        System.out.println(goodVsEvil.battle("1 0 0 0 0 0", "1 0 0 0 0 0 0"));
+//        System.out.println(goodVsEvil.battle("379 875 9329 3755 9578 1053", "5839 3227 8980 5551 1855 4488 1142"));
+        System.out.println(goodVsEvil.battle("8257 3064 3334 4644 5240 465", "1276 3522 238 1 1988 8857 1323"));
     }
 }
