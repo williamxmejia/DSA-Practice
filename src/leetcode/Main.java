c9ecf704ae62fd7192ae13eca77f5740d00edb5b
williamxmejia <willx1995@gmail.com>
2022-10-22 13:52:17 -0500

    feat: isBalanced, using stacks

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -1,165 +1,17 @@
 package leetcode;
 
+import java.util.LinkedList;
+
 //7 kyu
 public class Main {
     public static void main(String[] args) {
-//        FizzBuzz fizzBuzz = new FizzBuzz();
-//
-//        System.out.println(fizzBuzz.fizzBuzz(100));
+        LinkedList<Integer> linkedList = new LinkedList<>();
 
-//        SingleNumber singleNumber = new SingleNumber();
-//        System.out.println(singleNumber.SingleNumber(new int[]{1, 2, 2, 3, 3}));
-//        System.out.println(singleNumber.SingleNumber(new int[]{1, 1, 2, 4, 4}));
-//        System.out.println(singleNumber.SingleNumber(new int[]{2, 2, 5, 5, 1}));
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
 
-//        ReverseInteger reverseInteger = new ReverseInteger();
-//
-//        System.out.println(reverseInteger.reverseInteger(123));
-//        System.out.println(reverseInteger.reverseInteger(-123));
-//        System.out.println(reverseInteger.reverseInteger(120));
-
-//        FindDivisor findDivisor = new FindDivisor();
-//        System.out.println(findDivisor.numberOfDivisors(4));
-//        System.out.println(findDivisor.numberOfDivisors(30));
-//        System.out.println(findDivisor.numberOfDivisors(12));
-
-//        String[] a1 = {"hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"};
-//        String []a2 = {"cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"};
-//
-//        MaxDiffLength maxDiffLength = new MaxDiffLength();
-//
-////        System.out.println(a1.length);
-////        System.out.println(a2.length);
-//
-//        System.out.println(a1[0].length());
-//
-//        System.out.println(maxDiffLength.mxdiflg(a1, a2));
-
-//        Kata kata = new Kata();
-//        int [] arr = {1,2,3,4,3,2,1};
-//
-//        System.out.println(kata.findEvenIndex(arr));
-
-//        GoodVsEvil goodVsEvil = new GoodVsEvil();
-//        System.out.println(goodVsEvil.battle("1 1 1 1 1 1", "1 1 1 1 1 1 1"));
-//        System.out.println(goodVsEvil.battle("0 0 0 0 0 10", "0 1 1 1 1 0 0"));
-//        System.out.println(goodVsEvil.battle("1 0 0 0 0 0", "1 0 0 0 0 0 0"));
-//        System.out.println(goodVsEvil.battle("379 875 9329 3755 9578 1053", "5839 3227 8980 5551 1855 4488 1142"));
-//        System.out.println(goodVsEvil.battle("8257 3064 3334 4644 5240 465", "1276 3522 238 1 1988 8857 1323"));
-
-//        Solution solution = new Solution();
-//
-//        int [] nums = {0,1,0,3,12};
-//        int [] nums2 = {0};
-//        int [] nums3 = {0,0,1,2,0,4,5,234,563,234342,0,2,22,3};
-//
-//        solution.moveZeroes(nums);
-//        solution.moveZeroes(nums2);
-//        solution.moveZeroes(nums3);
-//        FirstRepeatingNumber firstRepeatingNumber = new FirstRepeatingNumber();
-//
-//        int [] nums = {2,5,1,2,3,5,1,2,4};
-//        int [] nums1 = {2,1,1,2,3,5,1,2,4};
-//        int [] nums2 = {1,2,3,4,5,6};
-//        int [] nums3 = {1,2,3,2,5,3};
-//
-//
-//        System.out.println(firstRepeatingNumber.FirstRepeatingNumber(nums));
-//        System.out.println("=================================================");
-//        System.out.println(firstRepeatingNumber.FirstRepeatingNumber(nums1));
-//        System.out.println(firstRepeatingNumber.FirstRepeatingNumber(nums2));
-//        System.out.println(firstRepeatingNumber.FirstRepeatingNumber(nums3));
-
-//        TwoSum twoSum = new TwoSum();
-//
-//        int [] nums = {1, 2, 4};
-//        int [] nums1 = {1, 3, 4,5,2};
-//
-//        System.out.println(Arrays.toString(twoSum.twoSum(nums, 3)));
-//        System.out.println(Arrays.toString(twoSum.twoSum(nums, 6)));
-
-//        ContainesDuplicates containesDuplicates = new ContainesDuplicates();
-//
-//        System.out.println(containesDuplicates.containsDuplicate(nums));
-//        System.out.println(containesDuplicates.containsDuplicate(nums1));
-//        System.out.println(containesDuplicates.containsDuplicate(nums2));
-//        System.out.println(containesDuplicates.containsDuplicate(nums3));
-
-//        LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
-//
-//        System.out.println(lengthOfLastWord.lengthOfLastWord("Hello World"));
-//        System.out.println(lengthOfLastWord.lengthOfLastWord("   fly me   to   the moon  "));
-//        System.out.println(lengthOfLastWord.lengthOfLastWord("luffy is still joyboy"));
-
-//        System.out.println("Hello\b ");
-//        System.out.println(("abc#d##c").charAt(1));
-
-//        BackspacesInString backspacesInString = new BackspacesInString();
-//
-//        System.out.println(backspacesInString.cleanString("abc#d##c"));
-//        System.out.println(backspacesInString.cleanString("abc##d######"));
-
-//        ConsonantValue consonantValue = new ConsonantValue();
-//
-//        System.out.println(consonantValue.solve("helloworld"));
-//        System.out.println(consonantValue.solve("strength"));
-
-
-//        ValidAnagram validAnagram = new ValidAnagram();
-//
-//        String s1 = "anagram";
-//        String t1 = "nagaram";
-//
-//        String s2 = "rat";
-//        String t2 = "car";
-//
-//        System.out.println(validAnagram.isAnagram(s1, t1));
-//        System.out.println(validAnagram.isAnagram(s2, t2));
-
-//        char [] list = {'h','e','l','l','o'};
-//        char [] list1 = {'w','i','l','l','i', 'a', 'm'};
-//
-//        ReversedString reversedString = new ReversedString();
-//
-//        reversedString.reverseString(list);
-//        reversedString.reverseString(list1);
-
-
-
-
-//        RunningSum runningSum = new RunningSum();
-
-//        int [] nums = {1, 2, 4};
-//        int [] nums1 = {1, 3, 4,5,2};
-
-//        System.out.println(runningSum.runningSum(nums));
-//        System.out.println(runningSum.runningSum(nums1));
-
-
-//        PivotIndex pivotIndex = new PivotIndex();
-//
-//        int [] nums = {1,7,3,6,5,6};
-//        int [] num1 = {1,2,3};
-//        int [] num2 = {2,1,-1};
-//
-//        System.out.println(pivotIndex.pivotIndex(nums));
-//        System.out.println(pivotIndex.pivotIndex(num1));
-//        System.out.println(pivotIndex.pivotIndex(num2));
-
-//        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3, null)));
-//
-//        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4, null)));
-//
-////        System.out.println(l1.next.val);
-//
-//        AddTwoNumbersII addTwoNumbers = new AddTwoNumbersII();
-//
-//        System.out.println(addTwoNumbers.addTwoNumbers(l1, l2).val);
-
-        PascalsTriangle pascalsTriangle = new PascalsTriangle();
-
-        System.out.println(pascalsTriangle.pascalsTriangle(5).toString());
-
+        System.out.println(linkedList.get(1));
 
     }
 }
