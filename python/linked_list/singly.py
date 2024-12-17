527cd90399b4a6e4d3f30b55d2a8a3863808444e
williamxmejia <willx1995@gmail.com>
2024-12-17 16:44:00 -0600

    fixing spacing

--- python/linked_list/singly.py
+++ python/linked_list/singly.py
@@ -1,29 +1,30 @@
 class ListNode:
-	def __init__(self, val):
-		self.val = val
-		self.next = None
+  def __init__(self, val):
+    self.val = val
+    self.next = None
 
 class LinkedList:
-	def __init__(self):
-		self.head = None
+  def __init__(self):
+    self.head = None
 
-	def append(self, data):
-		new_node = ListNode(data)
-		if not self.head:
-			self.head = new_node
-			return
-		last_node = self.head
-		while last_node.next:
-			last_node = last_node.next
-		last_node.next = new_node
+  def append(self, data):
+    new_node = ListNode(data)
+    if not self.head:
+      self.head = new_node
+      return
 
-	def print_list(self):
-		current_node =  self.head
+    last_node = self.head
+    while last_node.next:
+      last_node = last_node.next
+    last_node.next = new_node
 
-		while current_node:
-			print(current_node.val, end=" -> ")
-			current_node = current_node.next
-		print("None")
+  def print_list(self):
+    current_node =  self.head
+
+    while current_node:
+      print(current_node.val, end=" -> ")
+      current_node = current_node.next
+      print("None")
 
 l1 = LinkedList()
 l1.append(1)
